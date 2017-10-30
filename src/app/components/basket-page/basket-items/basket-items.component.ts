61128d0e32e09a6c43ed9a3003e3947a3a450865
Aleksandra <o.barecka@gmail.com>
2017-10-30 06:03:50 +0100

    added observer for user cart data to react on changes on cart-items component

--- src/app/components/basket-page/basket-items/basket-items.component.ts
+++ src/app/components/basket-page/basket-items/basket-items.component.ts
@@ -1,24 +1,33 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { CurrentUserDataService } from '../../../services-and-models/current-user-data.service';
+import { Subscription } from 'rxjs/Subscription';
 
 @Component({
   selector: 'app-basket-items',
   templateUrl: './basket-items.component.html',
   styleUrls: ['./basket-items.component.css']
 })
-export class BasketItemsComponent implements OnInit {
+export class BasketItemsComponent implements OnInit, OnDestroy {
   currentUserCart = [];
-    arr = [1,2,3,4];
+  cartDataSubscription: Subscription;
 
   constructor(private currentUserDataService: CurrentUserDataService) { }
 
   ngOnInit() {
     this.currentUserCart = this.currentUserDataService.getCartItems();
-    console.log(this.currentUserCart[0].product[0].name);
+    this.cartDataSubscription = this.currentUserDataService.userCartChanged.subscribe((cartData: any) => {
+      this.currentUserCart = cartData;
+      console.log("Subsciption data: ", this.currentUserCart)
+    });
   }
 
   removeFromCart(productId) {
     this.currentUserDataService.removeFromCart(productId);
+  }
+
+  ngOnDestroy() {
+    this.cartDataSubscription.unsubscribe();
+    console.log("cart item destroyed");
   }
 
 }
--- src/app/services-and-models/current-user-data.service.ts
+++ src/app/services-and-models/current-user-data.service.ts
@@ -1,9 +1,13 @@
 import { Injectable } from '@angular/core';
 import { ProductsService } from './products.service';
+import { Subject } from 'rxjs/Subject';
+
 
 @Injectable()
 export class CurrentUserDataService {
   userCartData = [];
+  /* to observe changes in user cart data */
+  userCartChanged = new Subject();
 
   constructor(private productsService: ProductsService) { }
 
@@ -13,12 +17,16 @@ export class CurrentUserDataService {
       amount: amount
     });
     console.log("ADD_Current cart data: ", this.userCartData);
+    /* inform about changes in user cart data */
+    this.userCartChanged.next(this.userCartData);
   }
 
   removeFromCart(indexToRemove): void {
     /* pass a place of item in cart arr, not a product id */
     this.userCartData.splice(indexToRemove, 1);
     console.log("REMOVE_Current cart data: ", this.userCartData);
+    /* inform about changes in user cart data */
+    this.userCartChanged.next(this.userCartData);
   }
 
   getCartItems() {
